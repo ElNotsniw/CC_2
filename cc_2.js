49acedd8c42deaf2bec3fd376070264c3bc68064
ElNotsniw <winstonjosh010604@gmail.com>
2025-01-21 18:59:39 -0500

    Task 1 - Store Inventory

---
+++ cc_2.js
@@ -0,0 +1,7 @@
+let products = ["Laptop", "Mouse", "Keyboard", "Headphones", "Mousepad"]
+products.push("Monitor")
+products.pop()
+let removed = (products.pop())
+
+console.log(products)
+console.log(removed)
