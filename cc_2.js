dd8ed684a8f09cdd0ec638e3ab6e33d03f0286bd
ElNotsniw <winstonjosh010604@gmail.com>
2025-01-21 20:21:26 -0500

    Task 2 - Student Scores

--- cc_2.js
+++ cc_2.js
@@ -5,3 +5,11 @@ let removed = (products.pop())
 
 console.log(products)
 console.log(removed)
+
+let scores = [52, 61, 75, 84, 90];
+scores[1] = 57;
+let totalscores = scores.reduce((acc, scores) => acc + scores, 0);
+let averagescore = totalscores / scores.length
+
+console.log("Updated Scores:", scores)
+console.log("Average Scores:", averagescore)
